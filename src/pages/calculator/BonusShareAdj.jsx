063acb4675642b6bfe7cc865c76157b2609bc2b7
Prashant Puri <ppuri7069@gmail.com>
2023-04-27 22:51:43 +0545

    Changed the UI of BonusShareAdjustment

--- src/pages/calculator/BonusShareAdj.jsx
+++ src/pages/calculator/BonusShareAdj.jsx
@@ -1,49 +1,80 @@
 import React, { useState } from 'react';
-import { Box, Button, Flex, FormControl, FormLabel, Input, VStack, Text } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    VStack,
+    UnorderedList,
+    ListItem,
+} from '@chakra-ui/react';
 
 function BonusShareAdj() {
-  const [marketPriceBefore, setMarketPriceBefore] = useState('');
-  const [bonusPercentage, setBonusPercentage] = useState('');
-  const [marketPriceAfter, setMarketPriceAfter] = useState('');
+    const [marketPriceBefore, setMarketPriceBefore] = useState('');
+    const [bonusPercentage, setBonusPercentage] = useState('');
+    const [result, setResult] = useState(null);
 
-  const calculatePrice = () => {
-    const priceBefore = parseFloat(marketPriceBefore);
-    const bonusPercent = parseFloat(bonusPercentage);
+    const calculatePrice = () => {
+        const marketPriceBeforeValue = parseFloat(marketPriceBefore);
+        const bonusPercentageValue = parseFloat(bonusPercentage);
 
-    if (isNaN(priceBefore) || isNaN(bonusPercent)) {
-      alert('Please enter valid numbers.');
-      return;
-    }
+        if (isNaN(marketPriceBeforeValue) || isNaN(bonusPercentageValue)) {
+            alert('Please enter valid input values.');
+            return;
+        }
 
-    const priceAfter = priceBefore / (1 + bonusPercent / 100);
-    setMarketPriceAfter(priceAfter.toFixed(2));
-  };
+        const marketPriceAfter = marketPriceBeforeValue / (1 + (bonusPercentageValue / 100));
 
-  return (
-    <Flex direction="column" align="center" justify="center" p={4}>
-      <VStack spacing={4}>
-        <FormControl>
-          <FormLabel>Market Price (Before Book Closure):</FormLabel>
-          <Input type="number" value={marketPriceBefore} onChange={(e) => setMarketPriceBefore(e.target.value)} />
-        </FormControl>
+        setResult({
+            userInputMarketPrice: marketPriceBeforeValue.toFixed(2),
+            userInputBonusPercentage: bonusPercentageValue.toFixed(2),
+            marketPriceAfterAdjustment: marketPriceAfter.toFixed(2),
+        });
+    };
 
-        <FormControl>
-          <FormLabel>% of Bonus Share:</FormLabel>
-          <Input type="number" value={bonusPercentage} onChange={(e) => setBonusPercentage(e.target.value)} />
-        </FormControl>
-
-        <Button colorScheme="teal" onClick={calculatePrice}>Calculate</Button>
-
-        {marketPriceAfter && (
-          <Box mt={4}>
-            <Text>Market Price (Before Book Closure): {parseFloat(marketPriceBefore).toFixed(2)}</Text>
-            <Text>% of Bonus Share: {parseFloat(bonusPercentage).toFixed(2)}%</Text>
-            <Text>Market Price after Right Adj: {marketPriceAfter}</Text>
-          </Box>
-        )}
-      </VStack>
-    </Flex>
-  );
+    return (
+        <Container maxW="container.md">
+            <VStack spacing={4} pt={8}>
+                <Heading as="h1">Bonus Share Adjustment Calculator</Heading>
+                <FormControl id="marketPriceBefore">
+                    <FormLabel>Market Price (Before Book Closure):</FormLabel>
+                    <Input
+                        type="text"
+                        value={marketPriceBefore}
+                        onChange={(e) => setMarketPriceBefore(e.target.value)}
+                    />
+                </FormControl>
+                <FormControl id="bonusPercentage">
+                    <FormLabel>% of Bonus Share:</FormLabel>
+                    <Input
+                        type="text"
+                        value={bonusPercentage}
+                        onChange={(e) => setBonusPercentage(e.target.value)}
+                    />
+                </FormControl>
+                <Button onClick={calculatePrice} colorScheme="blue">
+                    Calculate
+                </Button>
+                {result && (
+                    <Box>
+                        <Heading as="h2" size="lg">
+                            Details:
+                        </Heading>
+                        <UnorderedList>
+                            <ListItem>
+                                Market Price (Before Book Closure): {result.userInputMarketPrice}
+                            </ListItem>
+                            <ListItem>% of Bonus Share: {result.userInputBonusPercentage}</ListItem>
+                            <ListItem>Market Price after Right Adj: {result.marketPriceAfterAdjustment}</ListItem>
+                        </UnorderedList>
+                    </Box>
+                )}
+            </VStack>
+        </Container>
+    );
 }
 
 export default BonusShareAdj;
